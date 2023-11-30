b2cf954d7a6b51b7da0bf26301081181283882bd
Santito <santito@santito.es>
2023-11-30 19:55:46 +0100

    yes

--- twind.config.ts
+++ twind.config.ts
@@ -9,4 +9,19 @@ const customTwindClasses = {
 export default {
   selfURL: import.meta.url,
   plugins: customTwindClasses,
+  theme: {
+    colors: {
+      background: "#282A36",
+      "current-line": "#44475A",
+      foreground: "#F8F8F2",
+      comment: "#6272A4",
+      cyan: "#8BE9FD",
+      green: "#50FA7B",
+      orange: "#FFB86C",
+      pink: "#FF79C6",
+      purple: "#BD93F9",
+      red: "#FF5555",
+      yellow: "#F1FA8C",
+    },
+  },
 } as Options;
