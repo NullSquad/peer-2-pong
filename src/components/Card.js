4c0f343d3367dfbf716417a74ff3b7d2ba913b4a
Santito <68120901+San-tito@users.noreply.github.com>
2024-08-31 22:41:52 +0200

    twitter follow card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,33 @@
-import { html } from "zaphod";
+import { html, useState } from "zaphod";
 
 const Card = ({ children, image }) => {
-	return html`
-        	<h6>${children}</h6>
-        	<img src=${image}/>
-	`;
+  const [isFollowing, setIsFollowing] = useState(true);
+  const text = isFollowing ? "Siguiendo" : "Seguir";
+  const buttonClassName = isFollowing
+    ? "tw-followCard-button is-following"
+    : "tw-followCard-button";
+  const handleClick = () => {
+    setIsFollowing(!isFollowing);
+  };
+
+  return html`
+    <article class="tw-followCard">
+      <header class="tw-followCard-header">
+        <img class="tw-followCard-avatar" src=${image} />
+        <div class="tw-followCard-info">
+          <strong>${children}</strong>
+          <span class="tw-followCard-infoUserName">@${children}</span>
+        </div>
+      </header>
+
+      <aside>
+        <button class=${buttonClassName} onClick=${handleClick}>
+          <span className="tw-followCard-text">${text}</span>
+          <span className="tw-followCard-stopFollow">Dejar de seguir</span>
+        </button>
+      </aside>
+    </article>
+  `;
 };
 
 export default Card;
