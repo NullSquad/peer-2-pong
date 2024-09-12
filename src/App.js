1fbd3c377c0af1c56cceea4932c3befbf55829b4
Bautista Rodriguez Ramal <bautrodr@cbr9s5.42barcelona.com>
2024-09-12 17:20:53 +0200

    TicTacToe

--- src/App.js
+++ src/App.js
@@ -1,35 +1,66 @@
 import { html } from "zaphod";
+import { useState } from "zaphod";
 import Card from "./components/Card.js";
 
-const users = [
-  {
-    user: "sguzman",
-    image:
-      "https://cdn.intra.42.fr/users/b4c2641d99c5484f2cd86dd8b454c457/sguzman.png",
-  },
-  {
-    user: "bautrodr",
-    image:
-      "https://cdn.intra.42.fr/users/2cda6bee6a2954eb4612156dfcf1243b/bautrodr.png",
-  },
-  {
-    user: "deordone",
-    image:
-      "https://cdn.intra.42.fr/users/b87ca409e3426d93cff377a3c1c3f031/deordone.png",
-  },
-  {
-    user: "eralonso",
-    image:
-      "https://cdn.intra.42.fr/users/1c7b1d707503c67f6075d34920a1c83d/eralonso.jpg",
-  },
-];
+const TURNS = {
+	X: "X",
+	O: "O",
+};
+
+const Square = ({ children, isSelected, updateBoard, index }) => {
+	const className = isSelected ? "is-selected" : '';
+	console.log(className);
+
+	const handleClick = () => {
+		updateBoard(index);
+	}
+	return html`
+		<div onClick=${updateBoard} class="square ${className}">
+			${children}
+		</div>
+	`;
+};
+
 
 const App = () => {
-  return html`
-    ${users.map(
-      ({ user, image }) => html` <${Card} image=${image}> ${user} <//> `,
-    )}
-  `;
+
+	const [board, setBoard] = useState(Array(9).fill(null));
+
+	const [turn, setTurn] = useState(TURNS.X);
+
+	const updateBoard = (index) => {
+		const newBoard = [...board];
+		newBoard[index] = turn;
+		setBoard(newBoard);
+
+		const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
+		setTurn(newTurn);
+	}
+
+	return html`
+	<main className='board'>
+		<h1>Tic-Tac-Toe</h1>
+		<section className="game">
+			${board.map((_, index) => html`
+				<${Square} 
+					key=${index}
+					index=${index}
+					updateBoard=${updateBoard}
+				>
+				${board[index]}
+				<//>
+			`)}
+		</section>
+		<section className="turn">
+			<${Square} isSelected=${turn === TURNS.X}>
+				${TURNS.X}
+			<//>
+			<${Square} isSelected=${turn === TURNS.O}> 
+				${TURNS.O}
+			<//>
+		</section>
+		</main>
+	`;
 };
 
 export default App;
