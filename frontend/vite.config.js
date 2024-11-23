81d29d04029d8c36f533b01cc006fdfc65d6c740
Santito <68120901+San-tito@users.noreply.github.com>
2024-11-23 22:19:41 +0000

    delete cors

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
 
+const { BACKEND_PORT, FRONTEND_PORT } = process.env
+
 export default defineConfig({
   plugins: [preact()],
   server: {
     host: '0.0.0.0',
     proxy: {
       '/api': {
-        target: `http://backend:${process.env.BACKEND_PORT}`,
+        target: `http://backend:${BACKEND_PORT}`,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
@@ -16,6 +18,6 @@ export default defineConfig({
       usePolling: true,
     },
     strictPort: true,
-    port: parseInt(process.env.FRONTEND_PORT, 10) || 3000,
+    port: parseInt(FRONTEND_PORT, 10) || 3000,
   },
 })
