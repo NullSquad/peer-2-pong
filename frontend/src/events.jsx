bd985a090d168662c8bc53ab517d5370b0d7871d
ZenitsuTHB <starxyz12@gmail.com>
2024-11-28 23:15:11 +0100

    a little bit more simple

--- frontend/src/events.jsx
+++ frontend/src/events.jsx
@@ -1,51 +1,17 @@
 
-export const Event = () => { 
+export const Event = ({children}) => { 
     return ( 
     <>
-    {/*The Events components Main Container*/}
      <div className="slider-container">
-    
-      {/*League Container Starts*/}
       <div className="league-window-container">
-  
         <div className="time-container">
           league ends in : 00h00mn00s
         </div>
-
-        {/*league Window (container) Starts */}
-        
         <div className="league-window">
           League-Window
+            {children}
         </div>
-        
-        {/*league Window (container) Ends */}
-
-
       </div>
-      {/*League Container Ends*/}
-
-
-
-
-      {/*{/*Tournaments Container Starts
-      <div className="tournaments-window-container">
-        
-        <div className="time-container">
-          Tournaments ends in : 2d                        
-        </div>
-
-
-        <div className="tournament-window">
-          Tournament-Window
-        </div>
-
-      </div>
-      Tournaments Container Ends*/}
-
-
-
-
-    
     </div>
     </>);  
 };
