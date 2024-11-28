b30365afcfa6d2a74365da7172337a78847fb14a
Santi Guzman <sguzman@cbr1s1.42barcelona.com>
2024-11-28 18:49:16 +0100

    feat: router

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,9 +1,16 @@
+import Router from "preact-router";
+import Home from "./pages/Home";
 import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <main className="min-h-screen bg-gray-900 text-white">
-      <Login />
+      <Router>
+        <Login path="/" />
+        <Home path="/home" />
+        <NotFound default />
+      </Router>
     </main>
   );
 }
