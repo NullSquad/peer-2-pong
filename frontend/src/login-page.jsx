f4fdde02edf588e3e59ff16a795aff2b52fe53f5
david <davidmendezcarrillo01@gmail.com>
2024-11-28 15:56:32 +0100

    login button onclick event added

--- frontend/src/login-page.jsx
+++ frontend/src/login-page.jsx
@@ -1,8 +1,13 @@
 function Login() {
+
+    const handleClick = () => {
+      console.log("Login button has been clicked");
+    }
+
     return (
         <div className="min-h-screen flex flex-col items-center justify-center bg-gray-800 text-white">
           <h1 className="text-4xl font-bold mb-6">Peer2Pong</h1>
-          <button className="bg-primary px-8 py-2 rounded-lg text-white">
+          <button onClick={handleClick} className="bg-primary px-8 py-2 rounded-lg text-white">
             Login
           </button>
         </div>
