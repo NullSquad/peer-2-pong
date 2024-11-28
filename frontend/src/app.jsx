53010cdecd255685e8c66a96a66972bf6b19d3cd
david <davidmendezcarrillo01@gmail.com>
2024-11-28 14:24:39 +0100

    pending correcting colors and button funcionality

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,39 +1,12 @@
 import { useState, useEffect } from 'preact/hooks';
+import Login from "./login-page.jsx"
 
-export function App() {
-  const [message, setMessage] = useState('');
-
-  useEffect(() => {
-    async function fetchMessage() {
-      const response = await fetch('/api/');
-      const data = await response.json();
-      setMessage(data.message);
-    }
-    fetchMessage();
-  }, []);
-
+function App() {
   return (
     <>
-      <LoginPage />
+      <Login />
     </>
   );
 }
 
-function LoginPage() {
-  return (
-    <div class="flex justify-center items-center bg-gray-100">
-      <h1>Login Page</h1>
-      <LoginPageButton />
-    </div>
-  );
-}
-
-function LoginPageButton() {
-    return (
-        <button
-        class="block border-gray-500 rounded">
-          Login
-        </button>
-    );
-}
-
+export default App;
--- frontend/src/login-page.jsx
+++ frontend/src/login-page.jsx
@@ -1,5 +1,12 @@
-import { useState, useEffect } from 'preact/hooks';
+function Login() {
+    return (
+        <div className="min-h-screen flex flex-col items-center justify-center bg-gray-800 text-white">
+          <h1 className="text-4xl font-bold mb-6">Peer2Pong</h1>
+          <button className="bg-primary px-8 py-2 rounded-lg text-white">
+            Login
+          </button>
+        </div>
+      );
+};
 
-
-
-//export default LoginPage;
+export default Login;
