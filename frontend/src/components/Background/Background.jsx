15d130905bced890ab45706502366d1713912613
Nelly Naomy Castellanos Ruiz <ncastell@cbr2s6.42barcelona.com>
2024-11-28 18:46:58 +0100

    components header and dashboards

---
+++ frontend/src/components/Background/Background.jsx
@@ -0,0 +1,10 @@
+import { h } from 'preact';
+import './Background.css'; // Archivo de estilos
+
+export function Background() {
+  return (
+    <div className="background">
+      <div className="background-overlay"></div>
+    </div>
+  );
+}
