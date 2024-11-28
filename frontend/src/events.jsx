a58f77c898a186a13907347c0c18695375a6c3b0
ZenitsuTHB <starxyz12@gmail.com>
2024-11-28 18:50:40 +0100

    first commit for the first Event component

--- frontend/src/events.jsx
+++ frontend/src/events.jsx
@@ -1,8 +1,51 @@
+
 export const Event = () => { 
     return ( 
     <>
-    <div class="bg-primary">
-      <button>Open model</button>
+    {/*The Events components Main Container*/}
+     <div className="slider-container">
+    
+      {/*League Container Starts*/}
+      <div className="league-window-container">
+  
+        <div className="time-container">
+          league ends in : 00h00mn00s
+        </div>
+
+        {/*league Window (container) Starts */}
+        
+        <div className="league-window">
+          League-Window
+        </div>
+        
+        {/*league Window (container) Ends */}
+
+
+      </div>
+      {/*League Container Ends*/}
+
+
+
+
+      {/*{/*Tournaments Container Starts
+      <div className="tournaments-window-container">
+        
+        <div className="time-container">
+          Tournaments ends in : 2d                        
+        </div>
+
+
+        <div className="tournament-window">
+          Tournament-Window
+        </div>
+
+      </div>
+      Tournaments Container Ends*/}
+
+
+
+
+    
     </div>
-    </>)  
+    </>);  
 };
