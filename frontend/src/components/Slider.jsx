5d1a7ff372ad392131737076aa0d66ca4348c7d9
Bautista Rodriguez Ramal <bautrodr@cbr1s4.42barcelona.com>
2024-11-28 20:15:28 +0100

    tuqui components

--- frontend/src/components/Slider.jsx
+++ frontend/src/components/Slider.jsx
@@ -6,36 +6,30 @@ import 'swiper/css';
 export const Slider = ({children}) => {
   const count = Children.count(children);
   return (
-    <>
-    <article className="flex justify-center w-screen bg-blue py-8 px-8">
+    <article className="w-full bg-blue py-4 sm:py-6 md:py-8 px-2 sm:px-4 md:px-8">
       <Swiper
-      modules={[EffectCoverflow, EffectCards]}
-      loopFillGroupWithBlank={true}
-      cardsEffect={{
-        rotate: 10,
+        modules={[EffectCoverflow, EffectCards]}
+        loopFillGroupWithBlank={true}
+        cardsEffect={{
+          rotate: 10,
           stretch: 100,
           depth: 100,
           modifier: 1,
           slideShadows: false,
-      }}
-      effect={'cards'}
-      grabCursor={true}
-      // loop={true}
-      onSlideChange={() => console.log('slide change')}
-      onSwiper={(swiper) => console.log(swiper)}
-      className="flex justify-center items-center"
-      >
-      {
-        Children.map(children, (child, index) => (
-          <SwiperSlide  className="flex justify-center items-center"
-          key={index}>
-          {child}
+        }}
+        effect={'cards'}
+        grabCursor={true}
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={(swiper) => console.log(swiper)}
+        className="flex justify-center items-center"
+        >
+        {Children.map(children, (child, index) => (
+          <SwiperSlide key={index} className="flex justify-center items-center">
+            {child}
           </SwiperSlide>
-        ))
-      }
+        ))}
       </Swiper>
     </article>
-    </>
   );
 };
 
