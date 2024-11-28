7b81b8c276385dec2925266f790a21f1c1828a6e
Bautista Rodriguez Ramal <bautrodr@cbr1s4.42barcelona.com>
2024-11-28 18:31:50 +0100

    slider xdxd

--- frontend/src/slider.jsx
+++ frontend/src/slider.jsx
@@ -1,33 +1,40 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { EffectCards } from 'swiper/modules';
+import { EffectCards, EffectCoverflow } from 'swiper/modules';
 import { Children } from 'react';
 import 'swiper/css';
 
 export const Slider = ({children}) => {
-	const count = Children.count(children);
-	return (
-		<>
-			<article class="flex justify-center w-screen bg-white py-8 px-8">
-				<Swiper
-				effect={'cards'}
-		        grabCursor={true}
-		        modules={[EffectCards]}
-				onSlideChange={() => console.log('slide change')}
-				onSwiper={(swiper) => console.log(swiper)}
-				className="flex justify-center items-center"
-				>
-					{
-						Children.map(children, (child, index) => (
-			        	<SwiperSlide 
-						key={index} 
-						style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', }}
-						>
-							{child}
-						</SwiperSlide>
-			    		))
-					}
-		    	</Swiper>
-			</article>
-    	</>
-	);
+  const count = Children.count(children);
+  return (
+    <>
+    <article className="flex justify-center w-screen bg-blue py-8 px-8">
+    <Swiper
+    modules={[EffectCoverflow, EffectCards]}
+    loopFillGroupWithBlank={true}
+    cardsEffect={{
+      rotate: 10,
+        stretch: 100,
+        depth: 100,
+        modifier: 1,
+        slideShadows: false,
+    }}
+    effect={'cards'}
+    grabCursor={true}
+    // loop={true}
+    onSlideChange={() => console.log('slide change')}
+    onSwiper={(swiper) => console.log(swiper)}
+    className="flex justify-center items-center"
+    >
+    {
+      Children.map(children, (child, index) => (
+        <SwiperSlide  className="flex justify-center items-center"
+        key={index}>
+        {child}
+        </SwiperSlide>
+      ))
+    }
+    </Swiper>
+    </article>
+    </>
+  );
 };
