d5df1779e21ea0d1fb10b6a7f982a973e7c62dc4
Naomy Ruiz <109822565+ruiznaomy13@users.noreply.github.com>
2024-11-29 11:39:41 +0100

    Update Background.jsx
    Hecho con taildwindcss

--- frontend/src/components/Background/Background.jsx
+++ frontend/src/components/Background/Background.jsx
@@ -1,10 +1,9 @@
 import { h } from 'preact';
-import './Background.css'; // Archivo de estilos
 
 export function Background() {
   return (
-    <div className="background">
-      <div className="background-overlay"></div>
+    <div className="fixed inset-0 w-full h-full bg-[#1c2541] bg-[url('../../img/bgBlock.svg')] bg-repeat z-[-1]">
+      <div className="absolute inset-0 w-full h-full bg-black opacity-30"></div>
     </div>
   );
 }
