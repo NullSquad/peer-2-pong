8e8e59645848bf86f1fbbc2cebd421f3fb883517
Bautista Rodriguez <bautirodri@gmail.com>
2024-11-30 14:42:08 +0100

    taca

--- frontend/src/components/Events.jsx
+++ frontend/src/components/Events.jsx
@@ -1,46 +1,37 @@
 
-import leagueImage from "../../assets/league.svg"
-import tournamentImage from "../../assets/tournament.svg"
-
 const League = () => {
   return (
-    <div
-      className="flex h-full items-center justify-center parallelogram-tuta relative bg-cover bg-center shadow-lg mx-auto
+    <section
+      className="flex bg-[url('../../assets/league.svg')] h-full items-center justify-center parallelogram-tuta relative bg-cover bg-center shadow-lg mx-auto
         min-w-[16rem] min-h-[10rem] sm:min-w-[20rem] sm:min-h-[12rem]
         md:min-w-[24rem] md:min-h-[14rem]
         lg:min-w-[28rem] lg:min-h-[16rem]"
-      style={{
-        backgroundImage: `url(${leagueImage})`,
-      }}
     >
       {/* Centered Title */}
-      <div className="text-white text-center transform -translate-y-14 ">
+      <div className="text-white text-center transform -translate-y-8 sm:-translate-y-10 md:-translate-y-14">
         <h1 className="text-3xl sm:text-5xl lg:text-6xl font-bold drop-shadow-lg">
           LEAGUE
         </h1>
       </div>
-    </div>
+    </section>
   );
 };
 
 const Tournament = () => {
   return (
-    <div
-      className="flex h-full items-center justify-center parallelogram-tuta relative bg-cover bg-center shadow-lg mx-auto
+    <section
+      className="flex bg-[url('../../assets/tournament.svg')] h-full items-center justify-center parallelogram-tuta relative bg-cover bg-center shadow-lg mx-auto
         min-w-[16rem] min-h-[10rem] sm:min-w-[20rem] sm:min-h-[12rem]
         md:min-w-[24rem] md:min-h-[14rem]
         lg:min-w-[28rem] lg:min-h-[16rem]"
-      style={{
-        backgroundImage: `url(${tournamentImage})`,
-      }}
     >
       {/* Centered Title */}
-      <div className="text-white text-center transform -translate-y-14 w-fit ">
+      <div className="text-white text-center transform -translate-y-8 sm:-translate-y-10 md:-translate-y-14">
         <h1 className="text-2xl sm:text-4xl lg:text-5xl font-bold drop-shadow-lg">
           TOURNAMENT
         </h1>
       </div>
-    </div>
+    </section>
   );
 };
 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,7 +5,6 @@ import Event from "../components/Events";
 const Home = () => {
   return (
     <main className="min-h-screen bg-gray-800 text-white p-4">
-      <h1 className="text-2xl font-bold mb-4">Matches</h1>
       <Slider className="min-h-screen flex flex-col gap-6">
           <Event className="bg-primary size-40 flex items-center justify-center" >LEAGUE</Event>
           <Event className="bg-primary size-40 flex items-center justify-center" >TOURNAMENT</Event>
