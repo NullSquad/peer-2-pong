16ac6bdb65a2ba5a65b7d66fe4cc76c9d8b4c258
droied <deiord44santiagoov@gmail.com>
2024-12-03 18:53:16 +0100

    almost done

--- frontend/src/components/Usercard.jsx
+++ frontend/src/components/Usercard.jsx
@@ -1,12 +1,12 @@
 export const Usercard = ({ name, picture }) => {
 	return (
-		<article className="bg-primary border-4 -skew-x-6 flex flex-col gap-4 w-4/5 h-full" >
-			<div className="bg-primary-light w-full h-6"/>
-			<div className="flex flex-row p-4 skew-x-6 gap-4">
-				<img className="rounded-full border-4 w-14 sm:w-14 md:w-28 lg:w-32" src={picture}/>
-				<h2 className="p-4 flex items-center justify-center text-2xl sm:text-2xl md:text-3xl lg:text-5xl" > {name} </h2>
+		<article className="bg-primary border-4 -skew-x-12 flex flex-col w-4/5 h-full rounded-lg" >
+			<div className="bg-primary-light w-full h-3 lg:h-5"/>
+			<div className="flex flex-row p-4 skew-x-12 gap-4">
+				<img className="rounded-full shadow shadow-black border-4 w-14 sm:w-14 md:w-28 lg:w-32" src={picture}/>
+				<h2 className="p-4 flex items-center justify-center text-4xl sm:text-4xl md:text-5xl lg:text-7xl" > {name} </h2>
 			</div>
-			<div className="bg-primary-dark w-full h-6"/>
+			<div className="bg-primary-dark w-full h-3 lg:h-5"/>
 		</article>
 	);	
 }
