57583968f69ee3bd96b8e53749b6e18fb4bf6b7c
droied <deiord44santiagoov@gmail.com>
2024-12-03 23:04:01 +0100

    almost done

--- frontend/src/components/Usercard.jsx
+++ frontend/src/components/Usercard.jsx
@@ -1,12 +1,22 @@
+
+
 export const Usercard = ({ name, picture }) => {
 	return (
-		<article className="bg-primary border-4 -skew-x-12 flex flex-col w-4/5 h-full rounded-lg" >
-			<div className="bg-primary-light w-full h-3 lg:h-5"/>
-			<div className="flex flex-row p-4 skew-x-12 gap-4">
-				<img className="rounded-full shadow shadow-black border-4 w-14 sm:w-14 md:w-28 lg:w-32" src={picture}/>
-				<h2 className="p-4 flex items-center justify-center text-4xl sm:text-4xl md:text-5xl lg:text-7xl" > {name} </h2>
-			</div>
-			<div className="bg-primary-dark w-full h-3 lg:h-5"/>
+		<article className="flex flex-row w-4/5 h-2/6 rounded-lg">
+			<article className="object-conver bg-primary border-y-4 border-l-4 flex flex-col w-1/6 h-full rounded-lg">	
+					<div className="bg-primary-light w-full h-3 lg:h-5"/>
+					<div className="flex flex-row p-2">
+						<img className="rounded-full shadow shadow-black border-4 size-4/6" src={picture}/> 
+					</div>
+					<div className="bg-primary-dark w-full h-3 lg:h-5"/>
+			</article>
+			<article className="object-cover bg-primary border-y-4 border-r-4 -skew-x-12 flex flex-col w-5/6 h-full rounded-lg">
+				<div className="bg-primary-light w-full h-3 lg:h-5"/>
+				<div className="flex flex-row p-2 skew-x-12 ">
+					<h2 className="flex items-center justify-center text-1xl sm:text-4xl md:text-5xl lg:text-6xl" > {name} </h2>  
+				</div>
+				<div className="bg-primary-dark w-full h-3 lg:h-5"/>
+			</article>
 		</article>
 	);	
 }
