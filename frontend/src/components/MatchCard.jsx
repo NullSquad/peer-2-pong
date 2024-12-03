6dd9bf5ae6fa94b0b0ccf0cee49bcf254d67be06
Arnau Podadera Vives <apodader@cbr1s2.42barcelona.com>
2024-12-03 17:51:15 +0100

    tuta la ruina

--- frontend/src/components/MatchCard.jsx
+++ frontend/src/components/MatchCard.jsx
@@ -1,8 +1,14 @@
+import CrownIcon from "../assets/CrownIcon.svg";
 import { useState } from "preact/hooks";
 import CountdownTimer from "./CountdownTimer";
 
 export function MatchCard({ player1, player2, targetDate }) {
   const [phase, setPhase] = useState(1);
+
+  const result = {
+    blue: 13,
+    red: 11,
+  }
 
   const nextPhase = () => setPhase((prev) => (prev < 5 ? prev + 1 : 1));
   const prevPhase = () => setPhase((prev) => (prev > 1 ? prev - 1 : 5));
@@ -14,8 +20,8 @@ export function MatchCard({ player1, player2, targetDate }) {
       )}
       {phase === 2 && <Phase2 player1={player1} player2={player2} />}
       {phase === 3 && <Phase3 player1={player1} player2={player2} />}
-      {phase === 4 && <Phase4 />}
-      {phase === 5 && <Phase5 />}
+      {phase === 4 && <Phase4 player1={player1} player2={player2} />}
+      {phase === 5 && <Phase5 player1={player1} player2={player2} result={result}/>}
       
       <div className="absolute bottom-2 right-64 flex space-x-2">
         <button onClick={prevPhase} className="bg-gray-500 text-white p-2 rounded">
@@ -33,15 +39,15 @@ function Phase1({ player1, player2, targetDate }) {
   return (
     <div className="relative flex w-full max-w-4xl h-[86px] sm:h-[94px] md:h-28 overflow-visible bg-gray-800">
       {/* Card Body with clip-path */}
-      <div className="relative flex w-full h-14 sm:h-16 parallelogram">
+      <div className="relative flex w-full h-14 sm:h-16 -skew-x-[8deg]">
         {/* Player 1 Side */}
         <div className="flex items-center justify-center bg-blue-500 w-1/2 px-3 py-2 text-black">
           <img
             src={player1.image}
             alt={player1.name}
-            className="w-12 sm:w-14 rounded-full border-2 border-black mr-2"
+            className="w-12 sm:w-14 rounded-full border-2 skew-x-[8deg] border-black mr-2"
           />
-          <span className="text-sm md:text-lg font-bold">{player1.name}</span>
+          <span className="text-sm md:text-lg font-bold skew-x-[8deg]">{player1.name}</span>
         </div>
 
         {/* VS Section */}
@@ -58,14 +64,14 @@ function Phase1({ player1, player2, targetDate }) {
         <div className="w-1"></div>
 
         {/* Player 2 Side */}
-        <div className="flex items-center justify-center bg-red-500 w-1/2 px-3 py-2 text-black">
-          <span className="text-sm md:text-lg font-bold mr-2">
+        <div className="flex items-center justify-center bg-accent-red-light w-1/2 px-3 py-2 text-black">
+          <span className="text-sm md:text-lg skew-x-[8deg] font-bold mr-2">
             {player2.name}
           </span>
           <img
             src={player2.image}
             alt={player2.name}
-            className="w-12 sm:w-14 rounded-full border-2 border-black"
+            className="w-12 sm:w-14 rounded-full border-2 skew-x-[8deg] border-black"
           />
         </div>
       </div>
@@ -88,14 +94,14 @@ function Phase2({ player1, player2, targetDate }) {
   return (
     <div className="w-full">
       <div className="relative flex w-full max-w-4xl h-[86px] sm:h-[94px] md:h-28 overflow-visible bg-gray-800">
-        <div className="relative flex w-full h-14 sm:h-16 parallelogram">
+        <div className="relative flex w-full h-14 sm:h-16 -skew-x-[8deg]">
           <div className="flex items-center justify-center bg-blue-500 w-1/2 px-3 py-2 text-black">
             <img
               src={player1.image}
               alt={player1.name}
-              className="w-12 sm:w-14 rounded-full border-2 border-black mr-2"
+              className="w-12 sm:w-14 rounded-full skew-x-[8deg] border-2 border-black mr-2"
             />
-            <span className="text-sm md:text-lg font-bold">{player1.name}</span>
+            <span className="text-sm md:text-lg skew-x-[8deg] font-bold">{player1.name}</span>
           </div>
 
           <div
@@ -107,14 +113,14 @@ function Phase2({ player1, player2, targetDate }) {
 
           <div className="w-1"></div>
 
-          <div className="flex items-center justify-center bg-red-500 w-1/2 px-3 py-2 text-black">
-            <span className="text-sm md:text-lg font-bold mr-2">
+          <div className="flex items-center justify-center bg-accent-red-light w-1/2 px-3 py-2 text-black">
+            <span className="text-sm skew-x-[8deg] md:text-lg font-bold mr-2">
               {player2.name}
             </span>
             <img
               src={player2.image}
               alt={player2.name}
-              className="w-12 sm:w-14 rounded-full border-2 border-black"
+              className="w-12 sm:w-14 skew-x-[8deg] rounded-full border-2 border-black"
             />
           </div>
         </div>
@@ -124,7 +130,7 @@ function Phase2({ player1, player2, targetDate }) {
       <div className="flex justify-center items-center mt-2 gap-2 parallelogram">
         {/* Player 1 Score Controls */}
         <div className="flex items-center bg-blue-500 p-2 rounded">
-          <button className="bg-red-500 text-white px-2 py-1 rounded font-bold">-</button>
+          <button className="bg-accent-red-light text-white px-2 py-1 rounded font-bold">-</button>
           <span className="text-black text-2xl font-extrabold mx-2">6</span>
           <button className="bg-blue-700 text-white px-2 py-1 rounded font-bold">+</button>
         </div>
@@ -133,7 +139,7 @@ function Phase2({ player1, player2, targetDate }) {
         <div className="flex items-center bg-red-500 p-2 rounded">
           <button className="bg-blue-500 text-white px-2 py-1 rounded font-bold">+</button>
           <span className="text-black text-2xl font-extrabold mx-2">11</span>
-          <button className="bg-red-700 text-white px-2 py-1 rounded font-bold">-</button>
+          <button className="bg-accent-red-light  text-white px-2 py-1 rounded font-bold">-</button>
         </div>
 
         {/* Submit Button */}
@@ -151,8 +157,67 @@ function Phase4() {
   return <div>Fase 4: Análisis o resumen</div>;
 }
 
-function Phase5() {
-  return <div>Fase 5: Resultado final o estado completado</div>;
+function Phase5({ player1, player2, result }) {
+  const blueCardSize = result.blue > result.red ? "w-9/12" : "w-5/12";
+  const redCardSize = result.red > result.blue ? "w-9/12" : "w-5/12";
+  const crownPosition = result.blue > result.red ? "right-[80%]" : "right-[20%]";
+  const blueImagePosition = result.blue > result.red ? "right-[55%]" : "right-[82%]";
+  const redImagePosition =result.blue < result.red ? "left-[45%]" : "left-[72%]";
+
+  return (
+    <div className="relative flex w-full max-w-4xl h-28 overflow-visible bg-gray-800">
+      {/* Card Body */}
+      <div className="relative flex w-full h-14 sm:h-16 m-2 -skew-x-[8deg]">
+        {/* Blue card */}
+        <div
+          className={`relative flex items-center justify-between bg-blue-500 ${blueCardSize} px-3 py-2 text-black`}
+        >
+          {/* Blue score */}
+          <div
+            className="absolute sm:right-4 right-2 top-1/2 skew-x-[8deg] -translate-y-1/2 bg-black text-white p-1 px-2 rounded w-6 h-7 sm:w-7 sm:h-8 flex items-center justify-center" // absolute right-9"
+          >
+            <span className="font-bold text-sm skew-x-[8deg] sm:text-lg">{result.blue}</span>{" "}
+          </div>
+        </div>
+
+        {/* Space between blue and red */}
+        <div className="w-1"></div>
+
+        {/* Red card */}
+        <div
+          className={`relative flex items-center justify-center ${redCardSize} bg-accent-red-light px-3 py-2 text-black`}
+        >
+          {/* Red score */}
+          <div
+            className="absolute sm:left-4 left-2 top-1/2 skew-x-[8deg] -translate-y-1/2 bg-black text-white p-1 px-2 rounded w-6 h-7 sm:w-7 sm:h-8 flex items-center justify-center" // absolute right-9"
+          >
+            <span className="font-bold text-sm skew-x-[8deg] sm:text-lg">{result.red}</span>{" "}
+          </div>
+        </div>
+
+        {/* Player Images */}
+        <div className="flex items-center">
+          <img
+            src={player1.image}
+            alt={player1.name}
+            className={`absolute ${blueImagePosition} translate-x-1/2 skew-x-[8deg] w-12 sm:w-14 rounded-full border-2 border-black mr-3`}
+          />
+          <img
+            src={player2.image}
+            alt={player2.name}
+            className={`absolute ${redImagePosition} translate-x-1/2 skew-x-[8deg] w-12 sm:w-14 rounded-full border-2 border-black`}
+          />
+        </div>
+
+        {/* Crown Icon */}
+        <div
+          className={`absolute ${crownPosition} translate-x-[50%] skew-x-[8deg] flex justify-center w-1/3`}
+        >
+          <img src={CrownIcon} alt="Crown Icon" className="w-14 sm:w-16" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default MatchCard;
