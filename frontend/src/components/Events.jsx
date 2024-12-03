1e9d8656b01101c6d6a00bf6bb62b9df71e46e90
Bautista Rodriguez Ramal <bautrodr@cbr1s3.42barcelona.com>
2024-12-03 16:59:49 +0100

    HU terminada

--- frontend/src/components/Events.jsx
+++ frontend/src/components/Events.jsx
@@ -1,54 +1,3 @@
-// const Tournament = ({ children }) => {
-//   return (
-//     <section
-//       className="flex bg-[url('../../assets/tournament.svg')] h-full -skew-x-12 border-4 border-black items-center justify-center relative bg-cover bg-center shadow-lg mx-auto
-//           min-w-[16rem] min-h-[10rem] sm:min-w-[20rem] sm:min-h-[12rem]
-//           md:min-w-[24rem] md:min-h-[14rem]
-//           lg:min-w-[28rem] lg:min-h-[16rem]"
-//     >
-//       <div className="text-white text-center transform skew-x-12 -translate-y-10 sm:-translate-y-12 md:-translate-y-14 lg:-translate-y-16">
-//         <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold drop-shadow-lg">
-//           {children}
-//         </h1>
-//       </div>
-//     </section>
-//   );
-// };
-
-// const League = ({ children }) => {
-//   return (
-//     <section
-//       className="flex bg-[url('../../assets/tournament.svg')] h-full -skew-x-12 border-4 border-black items-center justify-center relative bg-cover bg-center shadow-lg mx-auto
-//           min-w-[16rem] min-h-[10rem] sm:min-w-[20rem] sm:min-h-[12rem]
-//           md:min-w-[24rem] md:min-h-[14rem]
-//           lg:min-w-[28rem] lg:min-h-[16rem]"
-//     >
-//       <div className="text-white text-center transform -translate-y-8 sm:-translate-y-10 md:-translate-y-12">
-//         <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold drop-shadow-lg">
-//           {children}
-//         </h1>
-//       </div>
-//     </section>
-//   );
-// };
-
-// const Event = ({ imageURL, children }) => {
-//   return (
-//     <section
-//       className={`flex bg-[url('${imageURL}')] h-full -skew-x-12 border-4 border-black items-center justify-center relative bg-cover bg-center shadow-lg mx-auto
-//       min-w-[16rem] min-h-[10rem] sm:min-w-[20rem] sm:min-h-[12rem]
-//       md:min-w-[24rem] md:min-h-[14rem]
-//       lg:min-w-[28rem] lg:min-h-[16rem]`}
-//     >
-//       <div className="text-white text-center transform skew-x-12 -translate-y-10 sm:-translate-y-12 md:-translate-y-14 lg:-translate-y-16">
-//         <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold drop-shadow-lg">
-//           {children}
-//         </h1>
-//       </div>
-//     </section>
-//   );
-// };
-//
 
 import CountdownTimer from "./CountdownTimer";
 
@@ -84,12 +33,11 @@ const Event = ({ type, isParticipating, status, children, targetDate }) => {
         `}
       >
           <div
-            className={`flex ${bgType} scale-[1.1] -translate-x-1 absolute inset-0 skew-x-6 bg-cover bg-bottom`}
+            className={`flex ${bgType}  scale-[1.1] -translate-x-1 absolute inset-0 skew-x-6 bg-cover bg-bottom`}
           />
-
-        <div className="flex justify-center translate-y-8">
+        <div className="flex justify-center translate-y-[1.2rem] md:translate-y-8">
           <div className="text-white skew-x-6">
-            <h1 className="text-2xl text-stroke sm:text-3xl md:text-4xl lg:text-5xl font-bold drop-shadow-lg">
+            <h1 className="text-2xl text-stroke-sm md:text-stroke sm:text-3xl md:text-4xl lg:text-5xl font-bold drop-shadow-lg">
               {children}
             </h1>
           </div>
