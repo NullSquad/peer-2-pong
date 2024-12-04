f1c6150105ead7c52a76686459afdf40ab289ab1
david <davidmendezcarrillo01@gmail.com>
2024-12-04 19:57:40 +0100

    logo change

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,13 +1,13 @@
 import { Link } from "preact-router/match";
-import Logo from "../assets/logo.svg";
+import Logo from "../assets/logov2.svg";
 
 const Login = () => {
   return (
     <main className="min-h-screen flex flex-col items-center justify-center bg-gray-800 text-white">
-      <img 
+         <img 
         src={Logo} 
         alt="Peer 2 Pong app Logo" 
-        className="w-auto max-w-xs h-auto mb-6"
+        className="w-auto max-w-xs h-auto mb-6 fill-white"
 	    />
       <Link href="/api/auth/" className="bg-primary-yellow shadow-yellow-50-700 px-12 py-5 rounded-sm text-white text-2xl text-stroke drop-shadow tracking-tighter font-bold">
           <h2>LOGIN</h2>
