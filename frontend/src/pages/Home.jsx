764caee34b03d2edf174d7604dfbed3d11b829fa
Nelly Naomy Castellanos Ruiz <ncastell@cbr1s2.42barcelona.com>
2024-12-04 17:48:13 +0100

    FIxed background and added separator component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,71 +3,72 @@ import MatchCard from "../components/MatchCard";
 import Slider from "../components/Slider";
 import Event from "../components/Events";
 import { Header } from "../components/Header";
+import { Separator } from "../components/Separator";
 
 const Home = () => {
   const { user } = useAuth();
 
   return (
-      <main className="relative text-white inset-0 w-full h-full bg-pattern pt-6 sm:pt-10">
+      <main className="relative inset-0 w-full h-full bg-pattern bg-cover pt-6 sm:pt-10">
         <Header />
-        <Slider className="min-h-screen flex flex-col gap-6">
-          <Event
-            type="league"
-            isParticipating={true}
-            status="ends"
-            targetDate="2024-12-04T16:59:59"
-          >
-            LEAGUE
-          </Event>
-          <Event
-            type="tournament"
-            isParticipating={false}
-            status="start"
-            targetDate="2024-12-31T23:59:59"
-          >
-            TOURNAMENT
-          </Event>
-        </Slider>
-
-        <section className="space-y-4 py-6">
-          <MatchCard
-            player1={{
-              name: user.login,
-              image: user.image,
-            }}
-            player2={{
-              name: user.login,
-              image: user.image,
-            }}
-            targetDate="2024-12-01T12:00:00"
-          />
-        </section>
-        <section className="space-y-6 py-6">
-          <MatchCard
-            player1={{
-              name: user.login,
-              image: user.image,
-            }}
-            player2={{
-              name: user.login,
-              image: user.image,
-            }}
-            targetDate="2024-11-29T12:00:00"
-          />
-        </section>
-        <section className="space-y-6 py-6">
-          <MatchCard
-            player1={{
-              name: user.login,
-              image: user.image,
-            }}
-            player2={{
-              name: user.login,
-              image: user.image,
-            }}
-            targetDate="2024-12-01T12:00:00"
-          />
-        </section>
+          <Slider className="min-h-screen flex flex-col gap-6">
+            <Event
+              type="league"
+              isParticipating={true}
+              status="ends"
+              targetDate="2024-12-04T16:59:59"
+            >
+              LEAGUE
+            </Event>
+            <Event
+              type="tournament"
+              isParticipating={false}
+              status="start"
+              targetDate="2024-12-31T23:59:59"
+            >
+              TOURNAMENT
+            </Event>
+          </Slider>
+          <Separator>Matches</Separator>
+          <section className="flex flex-col flex-wrap justify-center items-center mt-10">
+            <MatchCard
+              player1={{
+                name: user.login,
+                image: user.image,
+              }}
+              player2={{
+                name: user.login,
+                image: user.image,
+              }}
+              targetDate="2024-12-01T12:00:00"
+            />
+          </section>
+          <section className="flex flex-col flex-wrap justify-center items-center mt-10">
+            <MatchCard
+              player1={{
+                name: user.login,
+                image: user.image,
+              }}
+              player2={{
+                name: user.login,
+                image: user.image,
+              }}
+              targetDate="2024-11-29T12:00:00"
+            />
+          </section>
+          <section className="flex flex-col flex-wrap justify-center items-center mt-10">
+            <MatchCard
+              player1={{
+                name: user.login,
+                image: user.image,
+              }}
+              player2={{
+                name: user.login,
+                image: user.image,
+              }}
+              targetDate="2024-12-01T12:00:00"
+            />
+          </section>
       </main>
   );
 };
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,7 +2,7 @@ import { Link } from "preact-router";
 
 const Login = () => {
   return (
-	<main className="relative text-white inset-0 w-full min-h-screen bg-pattern pt-6 sm:pt-10 flex flex-col items-center justify-center">
+	<main className="relative inset-0 w-full h-full bg-pattern bg-cover text-white min-h-screen bg-pattern flex flex-col items-center justify-center">
       <h1 className="text-4xl font-bold mb-6">Peer2Pong</h1>
       <Link href="/api/auth/">
         <button className="bg-primary-yellow px-8 shadow-yellow-100-700 py-2 rounded-lg text-white">
