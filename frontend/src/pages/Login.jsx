3d2d026108ae0e4efa706a07061211b796b36817
Bautista Rodriguez Ramal <bautrodr@cbr2s3.42barcelona.com>
2024-12-04 20:10:06 +0100

    added logo

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,13 +1,19 @@
 import { Link } from "preact-router";
+import Logo from "../assets/logo.svg";
 
 const Login = () => {
   return (
-	<main className="relative inset-0 w-full h-full bg-pattern bg-cover text-white min-h-screen bg-pattern flex flex-col items-center justify-center">
-      <h1 className="text-4xl font-bold mb-6">Peer2Pong</h1>
-      <Link href="/api/auth/">
-        <button className="bg-primary-yellow px-8 shadow-yellow-100-700 py-2 rounded-lg text-white">
-          Login
-        </button>
+    <main className="relative inset-0 w-full h-full bg-pattern bg-cover text-white min-h-screen bg-pattern flex flex-col items-center justify-center">
+      <img
+        src={Logo}
+        alt="Peer 2 Pong app Logo"
+        className="w-auto max-w-xs h-auto mb-6 fill-white"
+      />
+      <Link
+        href="/api/auth/"
+        className="bg-primary-yellow shadow-yellow-50-700 px-12 py-5 rounded-sm text-white text-2xl text-stroke drop-shadow tracking-tighter font-bold"
+      >
+        <h2>LOGIN</h2>
       </Link>
     </main>
   );
