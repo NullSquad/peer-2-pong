420641c7a2cc1dfc7d4248dbf26a81c1ea49e386
Santito <68120901+San-tito@users.noreply.github.com>
2024-12-04 07:35:09 +0000

    login url

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,13 @@
 import { Link } from "preact-router";
+import { getLoginUrl } from "../services/authService";
 
 const Login = () => {
+  const loginUrl = getLoginUrl();
+
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-800 text-white">
       <h1 className="text-4xl font-bold mb-6">Peer2Pong</h1>
-      <Link href="/api/auth/">
+      <Link href={loginUrl}>
         <button className="bg-primary-yellow px-8 shadow-yellow-100-700 py-2 rounded-lg text-white">
           Login
         </button>
--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,11 +1,14 @@
 const { VITE_API_URI = "" } = import.meta.env;
 
 export const getSession = () => {
-  return fetch(`${VITE_API_URI}/auth/session`)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Failed to fetch session");
-      }
-      return response.json();
-    });
+  return fetch(`${VITE_API_URI}/auth/session`).then((response) => {
+    if (!response.ok) {
+      throw new Error("Failed to fetch session");
+    }
+    return response.json();
+  });
+};
+
+export const getLoginUrl = () => {
+  return `${VITE_API_URI}/auth/`;
 };
