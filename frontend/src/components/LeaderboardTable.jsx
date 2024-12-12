59508f5604a67a0e48b03f20706fe6d2f3f41f99
Nelly Naomy Castellanos Ruiz <ncastell@cbr6s6.42barcelona.com>
2024-12-12 12:25:13 +0100

    tabla hecha y tambien arreglos de  background, separator, header, etc

--- frontend/src/components/LeaderboardTable.jsx
+++ frontend/src/components/LeaderboardTable.jsx
@@ -4,18 +4,18 @@ import UserLeaderboard from "./UserLeaderboard";
 const LeaderboardTable = () => {
 	const { user } = useAuth();
 	// Se recogera de la BBDD
-	const users = Array(16).fill({ photo: user.image, login: user.login, score: 5});
+	const users = Array(20).fill({ photo: user.image, login: user.login, score: 5});
 
 	return (
 		<section className="flex justify-center items-start h-auto mt-16">
-			<div className="w-2/3 sm:w-6/7 border border-black text-center">
+			<div className="w-[90%] sm:w-2/3 sm:w-6/7 border border-black text-center">
 				<table className="w-full border-collapse border border-black">
 					<thead className="bg-yellow-600">
 						<tr>
-							<th className="border border-black p-2 w-1/4 min-w-[5rem]"> POSITION </th>
-							<th className="border border-black p-2 w-1/4 min-w-[5rem]"> PHOTO </th>
-							<th className="border border-black p-2 w-1/4 min-w-[5rem]"> LOGIN </th>
-							<th className="border border-black p-2 w-1/4 min-w-[5rem]"> SCORE </th>
+							<th className="border border-black p-2 w-1/4 "> POSITION </th>
+							<th className="border border-black p-2 w-1/4 "> PHOTO </th>
+							<th className="border border-black p-2 w-1/4 "> LOGIN </th>
+							<th className="border border-black p-2 w-1/4 "> SCORE </th>
 						</tr>
 					</thead>
 				</table>
--- frontend/src/components/UserLeaderboard.jsx
+++ frontend/src/components/UserLeaderboard.jsx
@@ -1,16 +1,24 @@
 const UserLeaderboard = ({ index, photo, login, score }) => {
     return (
-        <tr className={`${login === "user" ? "bg-yellow-300/60" : "bg-gray-200/80"} border border-black`}>
-			<td className="align-middle py-2 w-1/4"> {index + 1} </td>
-            <td className="align-middle py-2 w-1/4 text-center">
+        <tr
+			className={`border border-black ${
+				index === 10
+					? "bg-yellow-500/80"
+					: index % 2 === 0
+					? "bg-gray-200/70"
+					: "bg-gray-200/90"
+			}`}
+		>
+			<td className="align-middle py-1 w-1/4"> {index + 1} </td>
+            <td className="align-middle py-1 w-1/4 text-center">
                 <img
                     src={photo}
                     alt={`${login}'s avatar`}
-                    className={`${index <= 2 ? "border-2 border-gray-200" : "border-2 border-gray-700"} w-8 h-8 rounded-full  cursor-pointer mx-auto`}
+                    className={`${index <= 2 ? "border-[0.2em] border-yellow-400" : "border-2 border-gray-700"} w-10 h-10 rounded-full  cursor-pointer mx-auto`}
                 />
             </td>
-            <td className="align-middle py-2 w-1/4">{login}</td>
-            <td className="align-middle py-2 w-1/4">{score}</td>
+            <td className="align-middle py-1 w-1/4">{login}</td>
+            <td className="align-middle py-1 w-1/4">{score}</td>
         </tr>
     );
 };
