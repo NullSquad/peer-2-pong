594f962c09daa7ff7edc1699813e33142fb6304c
ZenitsuTHB <starxyz12@gmail.com>
2024-12-12 12:24:45 +0100

    to try table

--- frontend/src/components/Separator.jsx
+++ frontend/src/components/Separator.jsx
@@ -5,40 +5,39 @@ export const Separator = ({ children }) => {
   return (
     <header className="relative mb-10">
       {/* Primary Element */}
-      <div className="relative overflow-hidden ">
-        
-      <div
-        className="relative z-20 bg-yellow-500 text-white py-2 px-5 sm:px-10 ml-0
-        w-[50%] sm:w-[40%] md:w-[35%] lg:w-[30%] min-w-max flex justify-between items-center 
-        border-t-[8px] border-t-yellow-50 border-b-[8px] border-b-yellow-700 border-r-[4px]
-        border-r-black shadow-md transform -skew-x-[8deg]">
-        <h2
-          className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-bold skew-x-[8deg]"
-          style={{
-            WebkitTextStroke: "1px black",
-            color: "white",
-          }}
-        >
-          {children.toUpperCase()}
-        </h2>
-      </div>
-      </div>
 
-      {/* Secondary Element */}
-      <div
-        className="relative z-10 bg-yellow-500 text-white mt-5 py-1.5 px-5 sm:px-10 ml-0
-        w-[60%] sm:w-[50%] md:w-[45%] lg:w-[40%] flex justify-between items-center 
-        border-t-[8px] border-t-yellow-50 border-b-[8px] border-b-yellow-700 border-r-[4px]
-        border-r-black shadow-md transform -skew-x-[8deg]">
-      </div>
+<div className="relative overflow-hidden md:place-content-center">
+  {/* First Container */}
+  <div
+    className="relative z-20 bg-yellow-500 text-white py-2 px-5 sm:px-10 ml-0 
+    w-[50%] sm:w-[40%] md:w-[35%] lg:w-[30%] min-w-max flex justify-between 
+    items-center border-t-[8px] border-t-yellow-50 border-b-[8px] border-b-yellow-700 border-r-[4px] border-r-black shadow-md transform -skew-x-[8deg]"
+  >
+    <h2
+      className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-bold skew-x-[8deg]"
+      style={{
+        WebkitTextStroke: "1px black",
+        color: "white",
+      }}
+    >
+      {children.toUpperCase()}
+    </h2>
+  </div>
 
-      {/* Tertiary Element */}
-      <div
-        className="relative z-0 bg-yellow-500 text-white py-1.5 px-5 sm:px-10 ml-0 w-[70%]
-        sm:w-[60%] md:w-[55%] lg:w-[50%] flex justify-between items-center border-t-[8px]
-        border-t-yellow-50 border-b-[8px] border-b-yellow-700 border-r-[4px] border-r-black 
-        shadow-md transform -skew-x-[8deg]">
-      </div>
-    </header>
+	  {/* Second Container */}
+  	<div
+    	className="absolute z-0 bg-yellow-500 text-white py-1.5 px-5 sm:px-10 ml-0 
+    	w-[65%] sm:w-[55%] md:w-[50%] lg:w-[45%] min-w-max flex justify-between 
+    	items-center border-t-[8px] border-t-yellow-50 border-b-[8px] border-b-yellow-700 border-r-[4px] border-r-black shadow-md transform -skew-x-[8deg] -mt-[40px]"
+  	></div>
+
+	  {/* Third Container */}
+  	<div
+    	className="absolute z-10 bg-yellow-500 text-white py-1.5 px-5 sm:px-10 ml-0 
+    	w-[60%] sm:w-[50%] md:w-[45%] lg:w-[40%] min-w-max flex justify-between 
+    	items-center border-t-[8px] border-t-yellow-50 border-b-[8px] border-b-yellow-700 border-r-[4px] border-r-black shadow-md transform -skew-x-[8deg] -mt-[20px]"
+  	></div>
+	</div>
+   </header>
   );
 };
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -3,7 +3,8 @@ import Logo from "../assets/logo.svg";
 
 const Login = () => {
   return (
-    <main className="relative inset-0 w-full h-full bg-pattern bg-cover text-white min-h-screen bg-pattern flex flex-col items-center justify-center">
+    <main className="relative inset-0 w-full h-full bg-pattern bg-cover text-whitemin-h-screen
+	  				min-h-screen bg-pattern flex flex-col items-center justify-center">
       <img
         src={Logo}
         alt="Peer 2 Pong app Logo"
@@ -11,7 +12,8 @@ const Login = () => {
       />
       <Link
         href="/api/auth/"
-        className="bg-primary-yellow shadow-yellow-50-700 px-12 py-5 rounded-sm text-white text-2xl text-stroke drop-shadow tracking-tighter font-bold"
+        className="bg-primary-yellow shadow-yellow-50-700 px-12 py-5 rounded-smtext-white
+	  				text-white text-2xl text-stroke drop-shadow tracking-tighter font-bold"
       >
         <h2>LOGIN</h2>
       </Link>
