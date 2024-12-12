9d631814feffe9d8fe29cfd4e7ef72cfb7abea38
David Mendez Carrillo <50409052+damendez00@users.noreply.github.com>
2024-12-12 14:57:58 +0000

    adding update player score in competition

--- backend/src/controllers/matches.js
+++ backend/src/controllers/matches.js
@@ -88,6 +88,7 @@ const controller = {
     if (confirm) {
       confirm.status = "confirmed";
       confirm.date = new Date();
+      // update competition score here
     } else {
       confirm.status = "scheduled";
       confirm.players.forEach((player) => (player.reported = false));
