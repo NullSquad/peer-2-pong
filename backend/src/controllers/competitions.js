45315b3aa04ad61358fba4566a6892b73edec6d7
Santito <68120901+San-tito@users.noreply.github.com>
2024-12-13 00:54:06 +0000

    el cookeador

--- backend/src/controllers/competitions.js
+++ backend/src/controllers/competitions.js
@@ -40,9 +40,9 @@ const controller = {
   async getMatchesOfUser(id, userId) {
     return collection.aggregate([
       { $match: { _id: new ObjectId(id) } },
-      { $unwind: { path: "$players", preserveNullAndEmptyArrays: true } },
-      { $match: { "players.player": new ObjectId(userId) } },
-      { $project: { _id: 0, player: "$players.player", score: "$players.score" } },
+      { $unwind: "$matches" },
+      { $match: { "matches.players.player_id": new ObjectId(userId) } },
+      { $project: { _id: 0, matches: 1 } },
     ]).toArray();
   }
 };
