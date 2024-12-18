92233766c212f9f032713c88c0b3b224935bdd71
ruiznaomy13 <ruiznaomy13@gmail.com>
2024-12-18 15:17:04 +0100

    sintax

--- frontend/src/components/UserLeaderboard.jsx
+++ frontend/src/components/UserLeaderboard.jsx
@@ -2,8 +2,8 @@ import useAuth from "../hooks/useAuth";
 
 const UserLeaderboard = ({ index, photo, login, score }) => {
 	const { user } = useAuth();
-    return (
-        <tr
+return (
+	<tr
 			className={`${
 				// login === user.login
 				// 	? "bg-yellow-500/20"
@@ -13,31 +13,31 @@ const UserLeaderboard = ({ index, photo, login, score }) => {
 					: "bg-gray-200/10"
 			}`}
 		>
-            <td className="align-middle justify-center py-1 w-[25%]">
-                <span
-                    className={`inline-block w-[1.3em] h-[1.3em] sm:w-[1.5em] sm:h-[1.5em] items-center justify-center mr-2 ${
-                        index <= 2 ? "bg-yellow-400 rounded-full" : ""
-                    }`}
-                >
-                    {index + 1}
-                </span>
-                {login}
-            </td>
-            <td className="align-middle py-1 w-[15%] text-center">
-                <img
-                    src={photo}
-                    alt={`${login}'s avatar`}
-                    className={`w-8 h-8 sm:w-10 sm:h-10 rounded-full cursor-pointer mx-auto ${
-                        index <= 2 ? "border-[0.2em] border-yellow-400" : "border-[0.2em] border-gray-700"
-                    }`}
-                />
-            </td>
-            <td className="align-middle py-1 w-[15%]">{score}</td>
-            <td className="align-middle py-1 w-[15%]">{score}</td>
-            <td className="align-middle py-1 w-[15%]">{score}</td>
-            <td className="align-middle py-1 w-[15%]">{score}</td>
-        </tr>
-    );
+		<td className="align-middle justify-center py-1 w-[25%]">
+			<span
+				className={`inline-block w-[1.3em] h-[1.3em] sm:w-[1.5em] sm:h-[1.5em] items-center justify-center mr-2 ${
+				index <= 2 ? "bg-yellow-400 rounded-full" : ""
+				}`}
+			>
+				{index + 1}
+			</span>
+			{login}
+		</td>
+		<td className="align-middle py-1 w-[15%] text-center">
+			<img
+				src={photo}
+				alt={`${login}'s avatar`}
+				className={`w-8 h-8 sm:w-10 sm:h-10 rounded-full cursor-pointer mx-auto ${
+				index <= 2 ? "border-[0.2em] border-yellow-400" : "border-[0.2em] border-gray-700"
+				}`}
+			/>
+		</td>
+		<td className="align-middle py-1 w-[15%]">{score}</td>
+		<td className="align-middle py-1 w-[15%]">{score}</td>
+		<td className="align-middle py-1 w-[15%]">{score}</td>
+		<td className="align-middle py-1 w-[15%]">{score}</td>
+	</tr>
+);
 };
 
 export default UserLeaderboard;
