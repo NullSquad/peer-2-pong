1c4a719250443a55902064af3e4d00d28f0a4abd
david <davidmendezcarrillo01@gmail.com>
2024-12-18 20:48:19 +0100

    ok ok

--- backend/tests/user.test.js
+++ backend/tests/user.test.js
@@ -1,9 +1,24 @@
 import request from "supertest";
 import app from "../src/main.js";
 
+const api = request(app)
+
+test('wiwi', () => {
+    api
+        .get("/users")
+        .expect(200)
+})
+
 describe("User routes", () => {
     it("GET /users", async () => {
         const response = await request(app).get("/users");
-        expect(response.status).toBe(200);
+        expect(response.status).toBe(201);
+    });
+});
+
+describe("User routes", () => {
+    it("GET /users", async () => {
+        const response = await request(app).get("/users");
+        expect(response.status).toBe(202);
     });
 });
